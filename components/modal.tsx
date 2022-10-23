193817535d18b3bf61be95228e3c6f99ad159f0a
Josiah Mann <dev@josiahmann.com>
2022-10-23 14:34:12 -0500

    add express server

--- components/modal.tsx
+++ components/modal.tsx
@@ -61,7 +61,7 @@ export default function Modal({open, ok, onClose, title, children}) {
 									</button>
                                     <button
 										type="button"
-										className="btn-primary"
+										className="mt-3 mr-2 inline-flex w-full justify-center rounded-md border border-gray-300 bg-blue px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
 										onClick={ok}
 										ref={cancelButtonRef}
 									>
--- views/WebsitePanel.tsx
+++
@@ -1,8 +0,0 @@
-// create a new react component
-function WebsiteOverlay(): React.ReactElement {
-    return (
-        <h1>Here's the overlay</h1>
-    )
-}
-
-export default WebsiteOverlay;

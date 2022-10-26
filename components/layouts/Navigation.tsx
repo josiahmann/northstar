a653d5158a3796e4c0185c37cef18a7f4a5ad594
Josiah Mann <dev@josiahmann.com>
2022-10-25 23:38:29 -0500

    refactor profile dropdown nav remove headless/ui

--- components/layouts/Navigation.tsx
+++ components/layouts/Navigation.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import Router, { useRouter } from "next/router";
+import { useLocation } from "react-router-dom";
 
-const user = {
+const userData = {
 	name: "Tom Cook",
 	email: "tom@example.com",
 	imageUrl:
@@ -26,186 +27,188 @@ function classNames(...classes: string[]) {
 
 function HeaderComponent() {
 	const router = useRouter();
-	const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
-    const menuButtonRef = useRef(null);
-	
+	const [user, setUser] = useState(userData);
+	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+	const [profileMenuOpen, setProfileMenuOpen] = useState(false);
+	const menuRef = useRef(null);
+
+
+    const handleClickOutside = (event: Event) => {
+        if (menuRef.current && !menuRef?.current.contains(event.target)) {
+			setProfileMenuOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('click', handleClickOutside, true);
+        return () => {
+            document.removeEventListener('click', handleClickOutside, true);
+        };
+    }, []);
+
+	useEffect(() => {
+		if (profileMenuOpen) {
+			setProfileMenuOpen(!profileMenuOpen);
+		}
+	}, [router.asPath]);
+    
 	return (
-		<Disclosure as="nav" className="bg-white shadow-sm">
-			{({ open }) => (
-				<>
-					<div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
-						<div className="flex h-16 justify-between">
-							{/* Logo and top left nav */}
-                            <div className="flex">
-								<div className="flex flex-shrink-0 items-center">
-									<img
-										className="block h-8 w-auto lg:hidden"
-										src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-										alt="Your Company"
-									/>
-									<img
-										className="hidden h-8 w-auto lg:block"
-										src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-										alt="Your Company"
-									/>
-								</div>
-								<div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
-									{navigation.map((item) => (
-										<Link href={item.path} key={item.name}>
-											<a
-												className={classNames(
-													router.pathname === item.path
-														? "border-indigo-500 text-gray-900"
-														: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
-													"inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-												)}
-											>
-												{item.name}
-											</a>
-										</Link>
-									))}
-								</div>
-							</div>
-							
-                            {/* Profile and Notifications */}
-                            <div className="hidden sm:ml-6 sm:flex sm:items-center">
-								<button
-									type="button"
-									className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-								>
-									<span className="sr-only">View notifications</span>
-									<BellIcon className="h-6 w-6" aria-hidden="true" />
-								</button>
-
-								{/* Profile dropdown */}
-                                
-								<Menu as="div" className="relative ml-3">
-									{({ open }) => (
-										<>
-											<div>
-												<Menu.Button onClick={()=> setIsProfileMenuOpen(true)} ref={menuButtonRef} className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-													<span className="sr-only">Open user menu</span>
-													<img
-														className="h-8 w-8 rounded-full"
-														src={user.imageUrl}
-														alt=""
-													/>
-												</Menu.Button>
-											</div>
-
-											{isProfileMenuOpen  && (
-												<Transition
-													as={Fragment}
-													enter="transition ease-out duration-200"
-													enterFrom="transform opacity-0 scale-95"
-													enterTo="transform opacity-100 scale-100"
-													leave="transition ease-in duration-75"
-													leaveFrom="transform opacity-100 scale-100"
-													leaveTo="transform opacity-0 scale-95"
-												>
-													<Menu.Items
-														className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-													>
-														{userNavigation.map((item) => (
-															<Menu.Item key={item.name}>
-																{({ active }) => (
-																	<Link href={item.path}>
-																		<a
-																			className={classNames(
-																				router.pathname === item.path
-																					? "bg-gray-100"
-																					: "",
-																				"block px-4 py-2 text-sm text-gray-700"
-																			)}
-																		>
-																			{item.name}
-																		</a>
-																	</Link>
-																)}
-															</Menu.Item>
-														))}
-													</Menu.Items>
-												</Transition>
-											)}
-										</>
-									)}
-								</Menu>
-							</div>
-							<div className="-mr-2 flex items-center sm:hidden">
-								{/* Mobile menu button */}
-								<Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-									<span className="sr-only">Open main menu</span>
-									{open ? (
-										<XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-									) : (
-										<Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-									)}
-								</Disclosure.Button>
-							</div>
+		<div ref={menuRef} className="bg-white shadow-sm">
+			<div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+				<div className="flex h-16 justify-between">
+					{/* Logo and top left nav */}
+					<div className="flex">
+						<div className="flex flex-shrink-0 items-center">
+							<img
+								className="block h-8 w-auto lg:hidden"
+								src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+								alt="Your Company"
+							/>
+							<img
+								className="hidden h-8 w-auto lg:block"
+								src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+								alt="Your Company"
+							/>
+						</div>
+						<div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+							{navigation.map((item) => (
+								<Link href={item.path} key={item.name}>
+									<a
+										className={classNames(
+											router.pathname === item.path
+												? "border-indigo-500 text-gray-900"
+												: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
+											"inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+										)}
+									>
+										{item.name}
+									</a>
+								</Link>
+							))}
 						</div>
 					</div>
 
-					<Disclosure.Panel className="sm:hidden">
-						<div className="space-y-1 pt-2 pb-3">
-							{navigation.map((item) => (
-								<Disclosure.Button
-									key={item.name}
-									as="a"
-									href={item.path}
-									className={classNames(
-										router.pathname === item.path
-											? "bg-indigo-50 border-indigo-500 text-indigo-700"
-											: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800",
-										"block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-									)}
-									aria-current={item.current ? "page" : undefined}
-								>
-									{item.name}
-								</Disclosure.Button>
-							))}
-						</div>
-						<div className="border-t border-gray-200 pt-4 pb-3">
-							<div className="flex items-center px-4">
-								<div className="flex-shrink-0">
-									<img
-										className="h-10 w-10 rounded-full"
-										src={user.imageUrl}
-										alt=""
-									/>
-								</div>
-								<div className="ml-3">
-									<div className="text-base font-medium text-gray-800">
-										{user.name}
-									</div>
-									<div className="text-sm font-medium text-gray-500">
-										{user.email}
-									</div>
-								</div>
-								<button
-									type="button"
-									className="ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-								>
-									<span className="sr-only">View notifications</span>
-									<BellIcon className="h-6 w-6" aria-hidden="true" />
-								</button>
+					{/* Profile and Notifications */}
+					<div className="hidden sm:ml-6 sm:flex sm:items-center">
+						{/* Profile dropdown */}
+
+						<div className="relative ml-3">
+							<div
+								onClick={() => setProfileMenuOpen(!profileMenuOpen)}
+								className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+							>
+								<span className="sr-only">Open user menu</span>
+								<img
+									className="h-8 w-8 rounded-full"
+									src={user.imageUrl}
+									alt=""
+								/>
 							</div>
-							<div className="mt-3 space-y-1">
-								{userNavigation.map((item) => (
-									<Disclosure.Button
-										key={item.name}
-										as="a"
-										href={item.path}
-										className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
-									>
-										{item.name}
-									</Disclosure.Button>
-								))}
+
+							{profileMenuOpen && (
+								<Transition
+									show={profileMenuOpen}
+									as={Fragment}
+									enter="transition ease-out duration-200"
+									enterFrom="transform opacity-0 scale-95"
+									enterTo="transform opacity-100 scale-100"
+									leave="transition ease-in duration-75"
+									leaveFrom="transform opacity-100 scale-100"
+									leaveTo="transform opacity-0 scale-95"
+								>
+									<div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+										{userNavigation.map((item) => (
+											<Link key={item.name} href={item.path}>
+												<a
+													className={classNames(
+														router.pathname === item.path ? "bg-gray-100" : "",
+														"block px-4 py-2 text-sm text-gray-700"
+													)}
+												>
+													{item.name}
+												</a>
+											</Link>
+										))}
+									</div>
+								</Transition>
+							)}
+						</div>
+					</div>
+
+					<div className="-mr-2 flex items-center sm:hidden">
+						{/* Mobile menu button */}
+						<div className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+							<span className="sr-only">Open main menu</span>
+							{mobileMenuOpen ? (
+								<XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+							) : (
+								<Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/* Mobile menu, show/hide based on menu state. */}
+			<div id="mobile-menu" className="sm:hidden">
+				<div className="space-y-1 pt-2 pb-3">
+					{navigation.map((item) => (
+						<div
+							key={item.name}
+							as="a"
+							href={item.path}
+							className={classNames(
+								router.pathname === item.path
+									? "bg-indigo-50 border-indigo-500 text-indigo-700"
+									: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800",
+								"block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+							)}
+							aria-current={item.current ? "page" : undefined}
+						>
+							{item.name}
+						</div>
+					))}
+				</div>
+				<div className="border-t border-gray-200 pt-4 pb-3">
+					<div className="flex items-center px-4">
+						<div className="flex-shrink-0">
+							<img
+								className="h-10 w-10 rounded-full"
+								src={user.imageUrl}
+								alt=""
+							/>
+						</div>
+						<div className="ml-3">
+							<div className="text-base font-medium text-gray-800">
+								{user.name}
+							</div>
+							<div className="text-sm font-medium text-gray-500">
+								{user.email}
 							</div>
 						</div>
-					</Disclosure.Panel>
-				</>
-			)}
-		</Disclosure>
+						<button
+							type="button"
+							className="ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+						>
+							<span className="sr-only">View notifications</span>
+							<BellIcon className="h-6 w-6" aria-hidden="true" />
+						</button>
+					</div>
+					<div className="mt-3 space-y-1">
+						{userNavigation.map((item) => (
+							<div
+								key={item.name}
+								as="a"
+								href={item.path}
+								className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
+							>
+								{item.name}
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
 
